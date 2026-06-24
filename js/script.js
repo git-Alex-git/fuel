d12c79556195ffc8a4278ef213df3a6c3060c9f4
git-Alex-git <lacatart.08@gmail.com>
2026-06-24 12:57:58 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let gas = document.querySelectorAll(".btn")
+let res = document.querySelector('.res')
+
+
+for (let i = 0; i < gas.length; i++) {
+    gas[i].onclick = function () {
+        let am = document.querySelectorAll('.gass')[0].value
+        let num = this.getAttribute('data-price')
+        if (am === '') {
+            alert('використовуй нормально!')
+            return
+        }
+        res.innerHTML = `До сплати: ${am * num} грн`
+        gas.forEach(btn => btn.classList.remove('active'))
+        this.classList.add('active')
+
+    }
+}
+
+
+
